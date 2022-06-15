cdb47809990a7f2af7a7ce79c9af0568f61cbb9a
rahul2103 <rahulsingh2103@yahoo.com>
2022-06-15 22:30:53 +0530

    integrate aouth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :homes
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root "homes#index"
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
